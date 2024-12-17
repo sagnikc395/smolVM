f4652fa1fcf0c99c655990004b8ed48b52a00e08
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-12-17 18:01:08 +0530

    src: define reigster and memory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+// memory storage
+
+const MEMORY_MAX: usize = 1 << 16;
+
+//general purpose registers to perform any calculations
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+enum Register {
+    R0 = 0,
+    R1,
+    R2,
+    R3,
+    R4,
+    R5,
+    R6,
+    R7,
+    PC,
+    COND,
+    COUNT,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let memory: [u16; MEMORY_MAX] = [0; MEMORY_MAX];
 }
