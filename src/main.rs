cb51e5302547d0d25c489b97fce019f8305a7577
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-12-21 15:22:16 +0530

    src: added enums for register, opcodes and condn flags

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,43 @@ const MEMORY_MAX: usize = 1 << 16;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 enum Register {
-    R0 = 0,
-    R1,
-    R2,
-    R3,
-    R4,
-    R5,
-    R6,
-    R7,
-    PC,
-    COND,
-    COUNT,
+    RR0 = 0,
+    RR1,
+    RR2,
+    RR3,
+    RR4,
+    RR5,
+    RR6,
+    RR7,
+    RPC, //program counter
+    RCOND,
+    RCOUNT,
+}
+#[derive(Debug, Copy, Clone, PartialEq)]
+enum Opcodes {
+    OPBR = 0, //branch
+    OPADD,    //add
+    OPLD,     // load
+    OPST,     // store
+    OPJSR,    /* jump register */
+    OPAND,    /* bitwise and */
+    OPLDR,    /* load register */
+    OPSTR,    /* store register */
+    OPRTI,    /* unused */
+    OPNOT,    /* bitwise not */
+    OPLDI,    /* load indirect */
+    OPSTI,    /* store indirect */
+    OPJMP,    /* jump */
+    OPRES,    /* reserved (unused) */
+    OPLEA,    /* load effective address */
+    OPTRAP,   /* execute trap */
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+enum ConditionFlags {
+    FLPOS = 1 << 0, // p
+    FLZRO = 1 << 1, //z
+    FLNEG = 1 << 2, // N
 }
 
 fn main() {
